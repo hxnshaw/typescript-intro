e8152ad69ee5c773bb52e9ed8eb0d32b67dfc8f0
hxnshaw <eghosasere07@gmail.com>
2022-08-30 14:49:17 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log('Hello World');
+
+// let a = 1 + 2
+// let b = a + 3
+// let c = {
+// apple: a,
+// banana: b
+// }
+// let d = c.apple * 4
+
+function squareOf(n:number) {
+    return n*n
+}
+
+const z=squareOf(2)
+console.log(z);
